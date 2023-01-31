92f1e34dc1df549d7d1d09522c5f02b88de9ec2d
Prezcoder <fbouchar@student.42quebec.com>
2023-01-31 10:10:22 -0500

    final

--- Cursus/get_next_line/main.c
+++ Cursus/get_next_line/main.c
@@ -6,11 +6,12 @@
 /*   By: fbouchar <fbouchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 13:42:03 by fbouchar          #+#    #+#             */
-/*   Updated: 2023/01/30 12:07:51 by fbouchar         ###   ########.fr       */
+/*   Updated: 2023/01/31 09:40:58 by fbouchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 int	main(void)
 {
@@ -19,6 +20,10 @@ int	main(void)
 	fd = open("text.txt", O_RDONLY);
 	if (fd == -1)
 		return (1);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
 	close(fd);
