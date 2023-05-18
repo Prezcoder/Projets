3460a81b142b9d5d592af36c8da7c3d82533d89c
Prezcoder <fbouchar@student.42quebec.com>
2023-05-18 15:36:19 -0400

    MAJ

--- Cursus/minishell/parse/srcs/main.c
+++ Cursus/minishell/parse/srcs/main.c
@@ -1,6 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbouchar <fbouchar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/18 13:52:12 by fbouchar          #+#    #+#             */
+/*   Updated: 2023/05/18 14:55:40 by fbouchar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 int main()
 {
+	int		i;
+	int 	j;
+	// int		length;
+	// char 	*prompt;
+	char	*name;
+	t_data	data;
 
+	i = 0;
+	j = 0;
+	data.path = ft_split(getenv("PATH"), ':');
+	// prompt = ;
+	data.lcmd.argv = ft_split(readline(ttyname(0)), ' ');
+	while (data.path[j])
+	{
+		name = ft_strjoin(data.path[j], "/", 0);
+		name = ft_strjoin(name, data.lcmd.argv[0], 1);
+		i = access(name, X_OK);
+		if (i == 0)
+		{
+			data.lcmd.cmdpath = name;
+		}
+		ft_printf("%i", i);
+		j++;
+	}
 }
--- Cursus/minishell/parse/srcs/minishell.h
+++ Cursus/minishell/parse/srcs/minishell.h
@@ -1,6 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbouchar <fbouchar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/18 14:07:05 by fbouchar          #+#    #+#             */
+/*   Updated: 2023/05/18 14:37:25 by fbouchar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
 #include "libft/libft.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+
+typedef	struct	s_ltkn
+{
+	char			*token;
+	struct s_ltkn	*next;
+}				t_ltkn;
+
+typedef	struct	s_lcmd
+{
+	char			*cmdpath;
+	char			**argv;
+	int				builtin;
+	struct s_lcmd	*next;
+}				t_lcmd;
+
+typedef	struct	s_data
+{
+	t_ltkn	ltkn;
+	t_lcmd	lcmd;
+	char	**path;
+}				t_data;
 
 #endif
